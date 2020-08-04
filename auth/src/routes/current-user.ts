509ff1f585be8918e9663089c6a1011f6cd03a6d
Sanya <purvi.sanya@gmail.com>
2020-08-04 21:49:32 +0400

    more auth

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import { currentUser } from '../middlewares/current-user'
+import { requireAuth } from '../middlewares/require-auth'
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!')
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+    // if (!req.session || !req.session.jwt) { //shorthand=> !req.session?.jwt
+    //     return res.send({currentUser: null})
+    // }
+    res.send({
+        currentUser: req.currentUser || null
+    })
+    
 })
 
 export { router as currentUserRouter }
