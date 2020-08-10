7364bbb4615999b8030e7f6ab36207853dedd25c
Sanya <purvi.sanya@gmail.com>
2020-08-10 19:54:57 +0400

    add ticketing service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,9 @@ import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@git-tix-ss/common'
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
+import { updateTicketRouter } from './routes/update'
 
 const app = express()
 // traffic is going to be proxied here through ingress-nginx and we want
@@ -20,6 +23,9 @@ app.use(
 
 app.use(currentUser)
 app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
 
 app.all('*', async () => {
     throw new NotFoundError()
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,11 +2,12 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 import { app } from '../app'
 import request from 'supertest'
+import jwt from 'jsonwebtoken'
 
 declare global {
     namespace NodeJS {
         interface Global {
-            signin(): Promise<string[]>
+            signin(): string[]
         } 
     }
 }
@@ -37,18 +38,25 @@ afterAll(async () => {
     await mongoose.connection.close();
 })
 
-global.signin = async () => {
-    const email = 'test@test.com'
-    const password = 'password'
+global.signin = () => {
+    // build a JWT payload. { id, email }
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: 'test@test.com'
+    }
 
-    const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-        email, password
-    })
-    .expect(201)
+    // create the JWT
+    const token = jwt.sign(payload, process.env.JWT_KEY!)
 
-    const cookie = response.get('Set-Cookie')
+    // build session object { jwt: MY_JWT }
+    const session = { jwt: token }
 
-    return cookie;
+    // turn that session into JSON
+    const sessionJSON = JSON.stringify(session)
+
+    // take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64')
+
+    // return a string that's the cookie w the encoded data.
+    return [`express:sess=${base64}`]
 }
