89a4a599355767626a9fe6c02e19954053ae883d
Sanya <purvi.sanya@gmail.com>
2020-08-21 23:05:30 +0400

    new startup msgs

--- client/components/header.js
+++ client/components/header.js
@@ -5,7 +5,7 @@ export default ({ currentUser }) => {
     const links = [
         !currentUser && { label: 'Sign Up', href: '/auth/signup'},
         !currentUser && { label: 'Sign In', href: '/auth/signin'},
-        currentUser && { label: 'Sell Tickets', href: '/tickets/new'},
+        currentUser && { label: 'Sell Tix', href: '/tickets/new'},
         currentUser && { label: 'My Orders', href: '/orders'},
         currentUser && { label: 'Sign Out', href: '/auth/signout'}
     ]
