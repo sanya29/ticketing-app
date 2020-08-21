ef63b50af8d6130613ce5eeca8eb14ec89e10538
Sanya <purvi.sanya@gmail.com>
2020-08-21 19:48:31 +0400

    fix test

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,16 +1,10 @@
 import express from 'express';
-import { currentUser } from '@git-tix-ss/common'
+import { currentUser } from '@git-tix-ss/common';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser,(req, res) => {
-    // if (!req.session || !req.session.jwt) { //shorthand=> !req.session?.jwt
-    //     return res.send({currentUser: null})
-    // }
-    res.send({
-        currentUser: req.currentUser || null
-    })
-    
-})
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
 
-export { router as currentUserRouter }
+export { router as currentUserRouter };
